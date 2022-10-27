0886c309d0d210b12af094bec60524ccbbe03a68
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-27 11:27:27 +0900

    feat: Parameter 를 쿼리 형태로 받고 echo 하는 endpoint 생성

--- api/api/src/main/java/com/springboot/api/controller/GetController.java
+++ api/api/src/main/java/com/springboot/api/controller/GetController.java
@@ -1,2 +1,24 @@
-package com.springboot.api.controller;public class GetController {
+package com.springboot.api.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+    //http://localhost:8081/api/v1/get-api/hello
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello World";
+    }
+
+    @GetMapping(value="/variable1/{variable}")
+    public String getWithParameter(@PathVariable("variable") String parameter) {
+        return parameter;
+    }
+
+    @GetMapping(value = "/request1")
+    public String getWithQueryParam(@RequestParam String name, @RequestParam String email, @RequestParam String organisation) {
+        return String.format("Your name: %s\nYour email: %s\nYour organisation: %s\n", name, email, organisation);
+    }
 }
